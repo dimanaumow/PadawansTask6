a1cbcdbfd0239393e8059f5f68810f6e4b45406f
Dima Naumow <dimanaumow123@gmail.com>
2019-03-01 21:13:17 +0300

    second attempt

--- NumberFinder.cs
+++ NumberFinder.cs
@@ -12,68 +12,67 @@ namespace PadawansTask6
                 throw new ArgumentException();
             }
 
-            int count = 0;
-            while (number > 0)
+            if(number / 10 == 0)
             {
-                number /= 10;
-                count++;
+                return null; 
             }
 
-            int[] digits = new int[count];
-
-            int m = count - 1;
-            while (number > 0)
+            List<int> dig = new List<int>();
+            while(number > 0)
             {
-                digits[m] = number % 10;
-                number /= 10;
-                m--;
+                dig.Add(number % 10);
+                number = number / 10; 
             }
+            int[] digits = dig.ToArray();
+            Array.Reverse(digits);
 
             int term = 0;
             for (int i = digits.Length - 1; i >= 1; i--)
             {
                 if (digits[i] > digits[i - 1])
                 {
-                    int minInd = i - 1; 
-                    for(int j = i - 1; j < digits.Length; j++)
+                    int minInd = i - 1;
+                    for (int j = i - 1; j < digits.Length; j++)
                     {
-                        if(digits[j] > digits[i - 1])
+                        if (digits[j] > digits[i - 1])
                         {
-                            minInd = j; 
+                            minInd = j;
                         }
                     }
                     int temp = digits[minInd];
                     digits[minInd] = digits[i - 1];
                     digits[i - 1] = temp;
-                    term = i - 1; 
-                    break; 
+                    term = i - 1;
+                    break;
                 }
             }
 
-            Array.Sort(digits, term + 1, digits.Length - term);
-            //for (int i = term + 1; i < digits.Length - 1; i++)
-            //{
-            //    for (int j = i + 1; j < digits.Length; j++)
-            //    {
-            //        if (digits[i] > digits[j])
-            //        {
-            //            int temp = digits[i];
-            //            digits[i] = digits[j];
-            //            digits[j] = temp;
-            //        }
-            //    }
-            //}
+
+            for (int i = term + 1; i < digits.Length - 1; i++)
+            {
+                for (int j = i + 1; j < digits.Length; j++)
+                {
+                    if (digits[i] > digits[j])
+                    {
+                        int temp = digits[i];
+                        digits[i] = digits[j];
+                        digits[j] = temp;
+                    }
+                }
+            }
 
             int result = 0;
+            int power = 0; 
             for (int i = digits.Length - 1; i >= 0; i--)
             {
-                result += digits[i] * (int)Math.Pow(10, digits.Length - i - 1); 
+                result += digits[i] * (int)Math.Pow(10, power);
+                power++; 
             }
 
-            if(result == number)
-            {
-                return null; 
-            }
+            //if(result == number)
+            //{
+            //    return null; 
+            //}
             return result; 
         }
     }
--- Tests/PublicTest.cs
+++ Tests/PublicTest.cs
@@ -9,6 +9,13 @@ namespace PadawansTask6.Tests
         [TestCase(513, ExpectedResult = 531)]
         [TestCase(2017, ExpectedResult = 2071)]
         [TestCase(414, ExpectedResult = 441)]
+        [TestCase(34722641, ExpectedResult = 34724126)]
+        [TestCase(144, ExpectedResult = 414)]
+        [TestCase(1234321, ExpectedResult = 1241233)]
+        [TestCase(1234126, ExpectedResult = 1234162)]
+        [TestCase(3456432, ExpectedResult = 3462345)]
+        [TestCase(10, ExpectedResult = -1)]
+        [TestCase(20, ExpectedResult = -1)]
         public int? NextBiggerThanTests(int number) => NumberFinder.NextBiggerThan(number);
     }
 }
